88d0e621b1c5f76a7f75ea77bfed45e403df05f7
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-11-08 16:05:40 +0400

    got god gor

--- src/arsi.h
+++ src/arsi.h
@@ -5,6 +5,8 @@ N(export);
 void tail() __attribute__((section(".text.end")));
 void head() __attribute__((section(".text.begin")));
 N(tail) {}
+static N(got) { C(2); }
+static N(god) { C(1); }
 static N(gor) { C(0); }
 n_t Tail;
 N(σeτail) {
@@ -13,7 +15,7 @@ N(σeτail) {
   A(addr) C(1);
 }
 N(head) {
-  ((void)gor), ((void)Tail);
+  ((void)gor), ((void)god), ((void)got), ((void)Tail);
 
   R(n_t, impexp);
 
