7f22b5230ed4f925df061a24adf552921a8d0536
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-11-21 08:09:17 +0400

    can extend os_next

--- src/a_epoll.c
+++ src/a_epoll.c
@@ -9,11 +9,15 @@ da,                   L)IN(L,
 //
 l_address,            L)IN(L,
 l_bind,               L)IN(L,
+l_epoll_create,       L)IN(L,
 l_epoll_ctl,          L)IN(L,
+l_epoll_wait,         L)IN(L,
 l_setnoblock,         L)IN(L,
 l_socket,             L)IN(L,
-//
-os_next,              L)IN(L,
+ls_export,            L)IN(L,
+os_new,               L)n_t os_next_org;I(L,
+os_next, os_next_org, L)IN(L,
+os_queue,             L)IN(L,
 os_wordump,           L)int(*print)(const char*, ...);I(L,
 printf, print,   import);
 // clang-format on
@@ -27,22 +31,119 @@ struct state_s {
   p_t *dσ;
   int flag;
 };
-N(მთავარი) { A2(9, 6) C(1); }
-
-N(os_socket_n) { C(1); }
+N(მთავარი) { A2(export, ls_export) O; }
+N(sock_an) {
+  // TODO: we have drain connected so add to epoll
+}
+N(sock_da) {}
+N(sock_ara) {}
+N(sock) {
+  A6(sock_an, sock_da, sock_ara, 4090, wordCountOf(struct state_s), os_new) O;
+}
+N(os_socket_n) { A(0) O; }
 N(os_socket) { A3(l_socket, os_socket_n, da) O; }
 N(os_bind) { A(2) C(1); }
 N(os_listen) { A(3) C(1); }
 N(os_next_nn) {
-  print("aha\n");
+  print("os_next_nn\n");
   C(1);
 }
-N(os_next_n) { A3(os_next, os_next_nn, an) O; }
+N(os_next) {
+  print("os_next_n\n");
+  A3(os_next_org, os_next_nn, an) O;
+}
+// static void addtopoll(p_t *σ) {
+//   struct state_s *s = S(state_s, σ);
+//   struct epoll_event event;
+//   event.data.ptr = σ;
+//   event.events = EPOLLIN | EPOLLET;
+//   epoll_ctl(epoll_fd, EPOLL_CTL_ADD, s->fd, &event);
+//   SetNonblocking(s->fd);
+// }
+// N(addtopolln) {
+//   R(p_t *, nσ);
+//   addtopoll(nσ);
+//   A(nσ) C(1);
+// }
+// int os_epoll_sockets() { return 0; }
+// void os_epoll_wait(Q_t ms) {
+//   long ret = epoll_wait(epoll_fd, events, MAX_EVENT_NUMBER, ms);
+//   if (ret < 0)
+//     printf("epoll errorn"), exit(1);
+//   for (Q_t i = 0; i < ret; i++) {
+//     p_t *σ = events[i].data.ptr, *ο = σ[0].v;
+//     Q_t α = 0;
+//     q_t ρ = σ[2].q;
+//     struct state_s *s = S(state_s, σ);
+//
+//     if (s->flag) {
+//       printf("aa\n");
+//       struct sockaddr_in client_address;
+//       socklen_t client_addrlength = sizeof(client_address);
+//       long connfd =
+//           accept(s->fd, (struct sockaddr *)&client_address,
+//           &client_addrlength);
+//       printf("z\n");
+//       A10(0, connfd, mksocket, addtopolln, da, god, s->dσ, 2, os_queue, da4)
+//       X;
+//     } else if (events[i].events & EPOLLIN) {
+//       printf("bb\n");
+//     }
+//   }
+// }
+// void os_epoll_init() { epoll_fd = epoll_create(5); }
+//
+// // TODO: use As macro instead of composition after creation
+// static N(setσ) {
+//   R(p_t *, nσ);
+//   R(long, fd);
+//   R(long, flag);
+//   struct state_s *s = S(state_s, nσ);
+//   s->fd = fd;
+//   s->flag = flag;
+//   A(nσ) C(1);
+// }
+//
+// static N(os_socket_an) {
+//   R(p_t *, oσ);
+//   struct state_s *s = S(state_s, σ);
+//   s->dσ = oσ;
+//   addtopoll(σ);
+//   A5(σ, gor, s->dσ, 2, os_queue) X;
+// }
+// static N(os_socket_da) {}
+// static N(os_socket_ara) {}
+// N(mksocket) {
+//   R(Q_t, listen_fd);
+//   R(Q_t, flag);
+//   A10(flag, listen_fd, os_socket_an, os_socket_da, os_socket_ara, 0x1000,
+//       wordCountOf(struct state_s), os_new, setσ, da)
+//   O;
+// }
+// N(os_socket) {
+//   long listen_fd = socket(PF_INET, SOCK_STREAM, 0);
+//   if (listen_fd < 0) {
+//     printf("fail to create socket!\n");
+//     return C(2);
+//   }
+//   A3(1, listen_fd, mksocket) O;
+// }
+// ondata    d =
+// onconnect s = ondata s 'DATA' 1 os_on
+// exam      s = onconnect s 'CONN' 1 os_on
+// N(os_on) {
+//   R(Q_t, wc);
+//   R(Q_t, event);
+//   R(p_t *, nσ);
+//   struct state_s *s = S(state_s, nσ);
+//   s->fd = 0;
+//   A3(wc, event, nσ) O;
+// }
 
 // clang-format off
 EN(Tail,          
 os_bind,                L)EN(L,
-os_listen,              L)E(L,
-os_next, os_next_n,     L)EN(L,
+os_listen,              L)EN(L,
+os_next,                L)EN(L,
 os_socket,              L)EN(L,
 მთავარი,           export);
--- src/a_test_socket.c
+++ src/a_test_socket.c
@@ -11,16 +11,16 @@ da3,                  L)IN(L,
 
 ls_export,            L)IN(L,
 os_new,               L)IN(L,
-os_wordump,           L)int(*print)(const char*, ...);I(L,
+os_queue,             L)int(*print)(const char*, ...);I(L,
 printf, print,        L)IN(L,
 
 os_bind,              L)IN(L,
 os_listen,            L)IN(L,
 os_next,              L)IN(L,
-os_socket,       import)
-    // clang-format on
+os_socket,       import);
+// clang-format on
 
-    N(drain_an) {
+N(drain_an) {
   α--;
   os_next(T());
 }
@@ -35,8 +35,11 @@ N(drain) {
   C(1);
 }
 N(მთავარი) {
-  A2(export, ls_export) O;
-  //A10(os_socket, "127.0.0.1", 7000, os_bind, da3, drain, mkdrain, da2, os_listen, da) O;
+  A(os_next) O;
+  //A9(export, ls_export, god, ara, σ, 4, os_queue, os_next, da) O;
+  //A10(os_socket, "127.0.0.1", 7000, os_bind, da3, drain, mkdrain, da2,
+  //    os_listen, da)
+  //O;
 }
 
 // clang-format off
