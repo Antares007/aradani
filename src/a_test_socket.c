cbf128727807a33194a929dfd5bf011cfddaa021
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-12-02 16:25:14 +0400

    ident

--- src/a_test_socket.c
+++ src/a_test_socket.c
@@ -1,25 +1,25 @@
 #include "arsi.h"
 // clang-format off
 IN(gor,
-and,                    L)IN(L,
-and2,                   L)IN(L,
-and3,                   L)IN(L,
-and3or,                 L)IN(L,
-and4,                   L)IN(L,
-not,                    L)IN(L,
-notand,                 L)IN(L,
-or,                     L)IN(L,
+and,                        L)IN(L,
+and2,                       L)IN(L,
+and3,                       L)IN(L,
+and3or,                     L)IN(L,
+and4,                       L)IN(L,
+not,                        L)IN(L,
+notand,                     L)IN(L,
+or,                         L)IN(L,
 
-ls_export,              L)IN(L,
-os_new,                 L)IN(L,
-os_queue,               L)int(*print)(const char*, ...);I(L,
-printf, print,          L)IN(L,
+ls_export,                  L)IN(L,
+os_new,                     L)IN(L,
+os_queue,                   L)int(*print)(const char*, ...);I(L,
+printf, print,              L)IN(L,
 
-mkdrain,                L)IN(L,
-os_bind,                L)IN(L,
-os_listen,              L)IN(L,
-os_next,                L)IN(L,
-os_socket,              L)n_t შიდა_მთავარი;I(L,
+mkdrain,                    L)IN(L,
+os_bind,                    L)IN(L,
+os_listen,                  L)IN(L,
+os_next,                    L)IN(L,
+os_socket,                  L)n_t შიდა_მთავარი;I(L,
 მთავარი, შიდა_მთავარი, import);
 
 N(ondata) {
@@ -41,8 +41,7 @@ N(მთავარი) {
       os_socket, and,
       "127.0.0.1", 7000, os_bind, and3,
       drain, mkdrain, and2,
-      os_listen, and)
-  O;
+      os_listen, and) O;
 }
 
 // clang-format off
