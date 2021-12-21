dd8c7837401bd4331eaa9f89d891debb1594ece7
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-12-21 21:38:35 +0400

    remove extra macros

--- src/arsi.h
+++ src/arsi.h
@@ -1,29 +1,28 @@
 #pragma once
 #include "exportmacros.h"
 #include "oars.h"
-#define Mpith(n) void n(void *s, const char *name, void *addr, m_t ie)
-Mpith(import);
-M(export);
+void import(void **s, const char *name, void *addr, void(ie)());
+void export(void *, void (*)(), void (*)());
 void tail() __attribute__((section(".text.end")));
 void head() __attribute__((section(".text.begin")));
-M(tail) {}
+void tail(void *s, void(a)(), void(o)()) {}
 int cmp(const char *s1, const char *s2) {
   while (*s1 == *s2++)
     if (*s1++ == 0)
       return (0);
   return (*(unsigned char *)s1 - *(unsigned char *)--s2);
 }
+typedef void (*m_t)(void *, void (*)(), void (*)());
 static int imported = 0;
 
 static void imp_err(void **s) { ((void (*)())s[2])(s[0]); }
 
 #define I(Tail, Name, Addr, Head)                                              \
-  Mpith(Head) {                                                                \
+  void Head(void **s, const char *name, void *addr, m_t ie) {                  \
     if (cmp(#Name, name) == 0) {                                               \
       Addr = addr;                                                             \
       if ((Tail) == 0)                                                         \
-        imported = 1,                                                          \
-        export(((void **)s)[0], ((void **)s)[1], ((void **)s)[2]);             \
+        imported = 1, export(s[0], s[1], s[2]);                                \
       else                                                                     \
         ie(s, Tail, imp_err);                                                  \
     } else                                                                     \
@@ -32,7 +31,7 @@ static void imp_err(void **s) { ((void (*)())s[2])(s[0]); }
 #define IN(Tail, Name, Head)                                                   \
   n_t Name;                                                                    \
   I(Tail, Name, Name, Head)
-M(head) {
+void head(void *s, void(and_ray)(), void(or_ray)()) {
   volatile m_t t = tail;
   if (imported)
     export(s, and_ray, or_ray);
--- src/exportmacros.h
+++ src/exportmacros.h
@@ -1,7 +1,7 @@
 #pragma once
-#define M(name) void name(void *s, void(and_ray)(), void(or_ray)())
-typedef M((*m_t));
 #define L CAT(expimp_, __LINE__)
 #define E(Tail, Name, Addr, Head)                                              \
-  M(Head) { and_ray(s, #Name, Addr, Tail); }
+  void Head(void *s, void(and_ray)(), void(or_ray)()) {                        \
+    and_ray(s, #Name, Addr, Tail);                                             \
+  }
 #define EN(Tail, Name, Head) E(Tail, Name, Name, Head)
