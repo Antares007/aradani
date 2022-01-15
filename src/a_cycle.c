5d6495c258110ade053479bf0c62796dc50fc1b1
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-01-15 13:02:48 +0400

    remove σ

--- src/a_cycle.c
+++ src/a_cycle.c
@@ -14,44 +14,39 @@ os_queue,                 L)IN(L,
 //
 s_pith,              import);
 
-N(god) { C(1); }
-N(gor) { C(0); }
-
 N(p_or) {
-  R(p_t *, oσ);
+  R(p_t *, oο);
   print("p_or\n");
-  A5(σ, gor, oσ, 3, os_queue) O;
+  A5(ο, gor, oο, 3, os_queue) O;
 }
 N(p_and) {
-  R(p_t *, oσ);
+  R(p_t *, oο);
   R(Q_t, c);
   if (c % 100000000 == 0) print("p_and %ld\n", c);
-  A6(c + 1, σ, god, oσ, 3, os_queue) O;
+  A6(c + 1, ο, god, oο, 3, os_queue) O;
 }
 N(p_not) { print("p_not\n"); }
 N(mproducer) { A6(p_or, p_and, p_not, 0x700, 0x0, os_new) O; }
 
 N(c_or) {
-  R(p_t *, oσ);
+  R(p_t *, oο);
   print("c_or\n");
-  A5(σ, god, oσ, 3, os_queue) O;
+  A5(ο, god, oο, 3, os_queue) O;
 }
 N(c_and) {
-  R(p_t *, oσ);
+  R(p_t *, oο);
   R(Q_t, c);
   if (c % 100000000 == 1) print("c_and %ld\n", c);
-  A6(c + 1, σ, god, oσ, 3, os_queue) O;
+  A6(c + 1, ο, god, oο, 3, os_queue) O;
 }
 N(c_not)     { print("c_not\n"); }
 N(mconsumer) { A6(c_or, c_and, c_not, 0x700, 0x0, os_new) O; }
 
 N(მთავარი) { A7(mconsumer, gor, mproducer, and2, 3, os_queue, and2) O; }
 
-N(updater) {
-  A3(0, მთავარი, and2)
-  C(1);
+N(init) {
+  A3(0, მთავარი, and2) C(1);
 }
-void init() { updateσ(s_pith, updater); }
 
 EN(tail,
 s_pith,              export);
