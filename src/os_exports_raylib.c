cd6cdfe732dd84250e931f97232ed3f986d298b1
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-01-26 15:55:12 +0400

    sort raylib exports

--- src/os_exports_raylib.c
+++ src/os_exports_raylib.c
@@ -9,31 +9,9 @@ N(exports);
 #define L CAT(export_raylib_, __LINE__)
 // clang-format off
 ED(exports,
-
-rl_InitWindow, {
-  R(Q_t, width);
-  R(Q_t, height);
-  R(const char *, title);
-  (InitWindow(width, height, title), C(1));
-}, L)ED(L,
-
-rl_WindowShouldClose, {
-  C(!!WindowShouldClose());
-}, L)ED(L,
-
-rl_BeginDrawing, {
-  (BeginDrawing(), C(1));
-}, L)ED(L,
-
-rl_EndDrawing, {
-  (EndDrawing(), C(1));
-}, L)ED(L,
-
-rl_ClearBackground, {
-  struct Color *color = (void *)&ο[--α];
-  (ClearBackground(*color), C(1));
-}, L)ED(L,
-
-rl_CloseWindow, {
-  (CloseWindow(), C(1));
-}, exports_raylib)
+rl_BeginDrawing, { (BeginDrawing(), C(1)); }, L)ED(L,
+rl_ClearBackground, { struct Color *color = (void *)&ο[--α]; (ClearBackground(*color), C(1)); }, L)ED(L,
+rl_CloseWindow, { (CloseWindow(), C(1)); }, L)ED(L,
+rl_EndDrawing, { (EndDrawing(), C(1)); }, L)ED(L,
+rl_InitWindow, { R(Q_t, width); R(Q_t, height); R(const char *, title); (InitWindow(width, height, title), C(1)); }, L)ED(L,
+rl_WindowShouldClose, { C(!!WindowShouldClose()); }, exports_raylib)
