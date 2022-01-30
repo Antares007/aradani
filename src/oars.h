36b7dc46d52045c1eeec391dc729ae250c559f37
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-01-30 18:21:52 +0400

    update oars header only

--- src/oars.h
+++ src/oars.h
@@ -3,19 +3,19 @@
                                   .
                init               .           next
     .......................................................
-           σ[α]  C   ο[ρ]         .            C
+           ο[α]  C   σ[ρ]         .            C
              |   C     |          .            C
-       σ→NNNn○○○ ο→○○○○OAN        .     σ[α]   C ο[ρ]
+       ο→NNNn○○○ σ→○○○○OAN        .     ο[α]   C σ[ρ]
                  C                .       |    C |
-                 C                .  σ→NNN○○○○ ο→OANnOAN
+                 C                .  ο→NNN○○○○ σ→OANnOAN
                  C                .            C
                  C                .            C
                  C                .            C
-        ο[ρ+2] = არა/not | ο[ρ+1] = და/and | ο[ρ+0] = ან/or
+        σ[ρ+2] = არა/not | σ[ρ+1] = და/and | σ[ρ+0] = ან/or
 */
 typedef struct p_s {
   union {
-    void (*c)(struct p_s *, unsigned long, unsigned long);
+    void (*c)(struct p_s *ο, unsigned long α, struct p_s *σ, unsigned long ρ);
     struct p_s *p;
     void *v;
     char b;
@@ -28,7 +28,7 @@ typedef struct p_s {
     unsigned long Q;
   };
 } p_t;
-#define OARS p_t *ο, unsigned long α, unsigned long ρ
+#define OARS p_t *ο, unsigned long α, p_t *σ, unsigned long ρ
 #define CAT_(a, b) a##b
 #define CAT(a, b) CAT_(a, b)
 
@@ -40,16 +40,14 @@ typedef struct p_s {
     n##p(T());                                                                 \
   }                                                                            \
   N(n##p)
-#define T(n) n##ο, n##α, n##ρ
+#define T(n) n##ο, n##α, n##σ, n##ρ
 #define R(T, n) T n = (T)ο[--α].v
 #define Sa(T, ...) ((*((T *)&ο[α]) = ((T)__VA_ARGS__)), (α += wordCountOf(T))),
 #define Sr(T, n) T *n = ((T *)&ο[α -= wordCountOf(T)])
 
-#define C(r) ο[ρ + (r)].c(ο, α, ρ)
-#define O ο[α - 1].c(ο, α - 1, ρ)
-#define A(a) ο[α++].v = (void *)(a),
-#define X A2(os_next, and) O
-#define S(T, σ) ((T *)&σ[-wordCountOf(T)])
+#define C(r) ο[ρ + (r)].c(ο, α, σ, ρ)
+#define O    ο[α -   1].c(ο, α - 1, σ, ρ)
+#define A(a) ο[α++    ].v = (void *)(a),
 
 #include "a21.h"
 #include "sa21.h"
