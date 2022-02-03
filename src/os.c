1d7123e28e0e920cc6d6f75c61dbab0bd7c4d4b3
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-02-03 06:50:56 +0400

    use variadic int starter

--- src/os.c
+++ src/os.c
@@ -1,5 +1,6 @@
 #include "import_export.h"
 #include "oars.h"
+#include <stdarg.h>
 #include <stdio.h>
 
 N(os_run_arsi);
@@ -10,18 +11,18 @@ N(os_next);
 N(ada);
 N(exports_next);
 
-SP(ray_not) {}
-//SP(run) {
-//  --α;
-//  R(n_t, nar);
-//  --α;
-//  nar(T());
-//}
+// SP(run) {
+//   --α;
+//   R(n_t, nar);
+//   --α;
+//   nar(T());
+// }
 
 N(seven);
 
 S(and) { A(010) ada(T()); }
 
+SP(ray_not) {}
 SP(ray_and) {
   R(p_t *, oο);
   R(const char *, file_name);
@@ -32,13 +33,9 @@ SP(ray_oor) {}
 
 void os_hrtime_init();
 void init_os_next();
-
-int main(int argc, char **argv) {
-  if (argc < 2) {
-    printf("Would you mind adding the 'file_name' as an argument?\n");
-    return 8;
-  }
-  const char *file_name = argv[1];
+void os(Q_t s, ...) {
+  va_list args;
+  va_start(args, s);
   p_t σ[512], ο[512];
   Q_t α = 0, ρ = 512;
 
@@ -50,7 +47,14 @@ int main(int argc, char **argv) {
 
   init_os_next();
   os_hrtime_init();
+  while (s)
+    A(va_arg(args, void *)) s--;
+  va_end(args);
+  O;
+}
 
-  A7(file_name, ray_oor, os_next, ray_not, os_new, ray_and, and) O;
-  return 0;
+int main(int argc, char **argv) {
+  if (argc < 2)
+    return printf("Would you mind adding the 'file_name' as an argument?\n"), 8;
+  return os(7, argv[1], ray_oor, os_next, ray_not, os_new, ray_and, and), 0;
 }
