6b98da7968ff75e6d861441ed8b60b2181386379
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-02-12 03:40:09 +0400

    timeout

--- src/a_async.c
+++ src/a_async.c
@@ -1,24 +1,55 @@
 #include "arsi.h"
+#include "gotgod.h"
 // clang-format off
 IBS(                L)IN(L,
 os_hrtime,          L)IN(L,
-os_new,             L)IN(L,
 os_queue,           L)IN(L,
-os_wordump,         L)IN(L,
 //
 and,                L)IN(L,
 and2,               L)IN(L,
-and3,               L)IN(L,
-and4,               L)IN(L,
-and5,               L)IN(L,
-and6,               L)IN(L,
-and7,               L)IN(L,
-andor,              L)IN(L,
-notand,             L)IN(L,
-or,           imports);
+and2or,       imports);
 // clang-format on
+N(addQQ) {
+  R(Q_t, r);
+  R(Q_t, l);
+  A(l + r) C(1);
+}
+N(subQQ) {
+  R(Q_t, r);
+  R(Q_t, l);
+  A(l - r) C(1);
+}
+N(ltQQ) {
+  R(Q_t, r);
+  R(Q_t, l);
+  // print("%lu < %lu = %d\n",l,r, l<r);
+  if (l < r)
+    C(1);
+  else
+    C(0);
+}
+N(mulQQ) { R(Q_t, r); R(Q_t, l); A(l * r) C(1); }
+N(divQQ) { R(Q_t, r); R(Q_t, l); A(l / r) C(1); }
+N(rotate2) { R(void *, b); R(void *, a); A2(b, a) C(1); }
+N(rotate3) { R(void *, c); R(void *, b); R(void *, a); A3(b, c, a) C(1); }
+N(dup) { R(void *, a); A2(a, a) C(1); }
+N(prnQ) { R(Q_t, v); print("%lu\n", v); C(1); }
 
-N(მთავარი) { A3(os_hrtime, os_wordump, and) O; }
+S(setTimeout_n) {
+  R(Q_t, time);
+  A10(os_hrtime, time, ltQQ, and2, time, setTimeout_n, dot, and2or, ο, os_queue) O;
+}
+N(setTimeout) { A5(os_hrtime, addQQ, and, setTimeout_n, and) O; }
+
+N(s10) { A13( os_hrtime, dup, and,
+              rotate3, and,
+              subQQ, and,
+              prnQ, and,
+              s10, and,
+              1000, setTimeout) O; }
+
+N(მთავარი) { A3(os_hrtime, s10, and) O; }
+
 void init() { print("\ninit %s\n", __FILE__); }
 
 // clang-format off
--- src/os_hrtime.c
+++ src/os_hrtime.c
@@ -1,12 +1,14 @@
 #include "oars.h"
 #include <stdint.h>
+#include <string.h>
 #include <time.h>
 
 static clock_t clock_id;
-
 struct timespec t;
+
 N(os_hrtime) {
-  A((clock_gettime(clock_id, &t) ? 0 : t.tv_sec * (uint64_t)1e9 + t.tv_nsec))
+  bzero(&t, sizeof(struct timespec));
+  A((clock_gettime(clock_id, &t) ? 0 : t.tv_sec * (uint64_t)1e9 + t.tv_nsec)/1000000)
   C(1);
 }
 void os_hrtime_init() {
