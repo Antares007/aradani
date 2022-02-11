cc6e7dcbb9c05d8c25bf81dd61706ddabf5cdff6
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-02-12 03:42:49 +0400

    clean

--- src/a_async.c
+++ src/a_async.c
@@ -8,32 +8,16 @@ os_queue,           L)IN(L,
 and,                L)IN(L,
 and2,               L)IN(L,
 and2or,       imports);
-// clang-format on
-N(addQQ) {
-  R(Q_t, r);
-  R(Q_t, l);
-  A(l + r) C(1);
-}
-N(subQQ) {
-  R(Q_t, r);
-  R(Q_t, l);
-  A(l - r) C(1);
-}
-N(ltQQ) {
-  R(Q_t, r);
-  R(Q_t, l);
-  // print("%lu < %lu = %d\n",l,r, l<r);
-  if (l < r)
-    C(1);
-  else
-    C(0);
-}
-N(mulQQ) { R(Q_t, r); R(Q_t, l); A(l * r) C(1); }
-N(divQQ) { R(Q_t, r); R(Q_t, l); A(l / r) C(1); }
+
+N(addQQ)   { R(Q_t, r); R(Q_t, l); A(l + r) C(1); }
+N(subQQ)   { R(Q_t, r); R(Q_t, l); A(l - r) C(1); }
+N(ltQQ)    { R(Q_t, r); R(Q_t, l); C(l < r); }
+N(mulQQ)   { R(Q_t, r); R(Q_t, l); A(l * r) C(1); }
+N(divQQ)   { R(Q_t, r); R(Q_t, l); A(l / r) C(1); }
 N(rotate2) { R(void *, b); R(void *, a); A2(b, a) C(1); }
 N(rotate3) { R(void *, c); R(void *, b); R(void *, a); A3(b, c, a) C(1); }
-N(dup) { R(void *, a); A2(a, a) C(1); }
-N(prnQ) { R(Q_t, v); print("%lu\n", v); C(1); }
+N(dup)     { R(void *, a); A2(a, a) C(1); }
+N(prnQ)    { R(Q_t, v); print("%lu\n", v); C(1); }
 
 S(setTimeout_n) {
   R(Q_t, time);
