22e8ec449d3ee908339151ea13fd8caeae54169a
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-02-12 09:53:22 +0400

    add lookahead (la)

--- src/a_parse.c
+++ src/a_parse.c
@@ -14,47 +14,69 @@ and5,               L)IN(L,
 and6,               L)IN(L,
 and7,               L)IN(L,
 andor,              L)IN(L,
-notand,             L)IN(L,
+andor2,             L)IN(L,
+not2and,            L)IN(L,
 or,           imports);
 
-// 0xxxxxxx
-// 110xxxxx	10xxxxxx
-// 1110xxxx	10xxxxxx	10xxxxxx
-// 11110xxx	10xxxxxx	10xxxxxx	10xxxxxx
-//struct u8cp {
-//  bool (*f)(uint32_t);
-//  template <typename O> void operator()(O o, const char *in) const {
-//    if ((in[0] & 0x80) == 0)
-//      if (f(in[0]))
-//        o(1);
-//      else
-//        o(error_ray_v, -1);
-//    else if ((in[1] & 0xc0) != 0x80)
-//      o(error_ray_v, -2);
-//    else if ((in[0] & 0xe0) == 0xc0)
-//      if (f((0x1f & in[0]) << 6 | (0x3f & in[1])))
-//        o(2);
-//      else
-//        o(error_ray_v, -3);
-//    else if ((in[2] & 0xc0) != 0x80)
-//      o(error_ray_v, -4);
-//    else if ((in[0] & 0xf0) == 0xe0)
-//      if (f((0x0f & in[0]) << 12 | (0x3f & in[1]) << 6 | (0x3f & in[2])))
-//        o(3);
-//      else
-//        o(error_ray_v, -5);
-//    else if ((in[3] & 0xc0) != 0x80)
-//      o(error_ray_v, -6);
-//    else if ((in[0] & 0xf8) == 0xf0)
-//      if (f((0x07 & in[0]) << 18 | (0x3f & in[1]) << 12 | (0x3f & in[2]) << 6 |
-//            (0x3f & in[3])))
-//        o(4);
-//      else
-//        o(error_ray_v, -7);
-//    else
-//      o(error_ray_v, -8);
-//  }
-//};
+/*
+0xxxxxxx
+110xxxxx 10xxxxxx
+1110xxxx 10xxxxxx 10xxxxxx
+11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+0x0f      1111
+0x1f    1 1111
+0x3f   11 1111
+0x80 1000 0000
+0xc0 1100 0000
+0xe0 1110 0000
+0xf0 1111 0000
+*/
+// clang-format off
+N(la) {
+  R(q_t,          p);
+  R(q_t,          l);
+  R(const char *, b);
+  
+  if (p > l - 1)
+    A3(b, l, p) C(2);
+  else if (p + 0 < l && (b[p + 0] & 0x80) == 0x00)
+    A((Q_t)b[p + 0]) A3(b, l, p + 1) C(1);
+  else if (p > l - 2 || (b[p + 1] & 0xc0) != 0x80)
+    A3(b, l, p) C(2);
+  else if ((b[p + 0] & 0xe0) == 0xc0)
+    A((0x1f & (Q_t)b[p + 0]) << 6
+    | (0x3f & (Q_t)b[p + 1])) A3(b, l, p + 2) C(1);
+  else if (p > l - 3 || (b[p + 2] & 0xc0) != 0x80)
+    A3(b, l, p) C(2);
+  else if ((b[p + 0] & 0xf0) == 0xe0)
+    A((0x0f & (Q_t)b[p + 0]) << 12
+    | (0x3f & (Q_t)b[p + 1]) << 6
+    | (0x3f & (Q_t)b[p + 2])) A3(b, l, p + 3) C(1);
+  else if (p > l - 4 || (b[p + 3] & 0xc0) != 0x80)
+    A3(b, l, p) C(2);
+  else if ((b[p + 0] & 0xf8) == 0xf0)
+    A((0x07 & (Q_t)b[p + 0]) << 18
+    | (0x3f & (Q_t)b[p + 1]) << 12
+    | (0x3f & (Q_t)b[p + 2]) << 6
+    | (0x3f & (Q_t)b[p + 3])) A3(b, l, p + 4) C(1);
+  else
+    A3(b, l, p) C(2);
+}
+// clang-format off
+Q_t cslen(const char*cs){
+  Q_t len = 0;
+  while(cs[len]) len++;
+  return len;
+}
+N(მთავარი) {
+  const char*cs = "აaბb";
+  A4(cs, cslen(cs), 0, la)
+  A2(la, and)
+  A2(la, and)
+  A2(la, and)
+  A4(0xabcd, os_wordump, os_wordump, not2and) O;
+}
+static void init() {}
 
 //N(Ο);                      
 //N(Μ);
@@ -157,9 +179,6 @@ or,           imports);
 // CombiningChar ::= [#x0300-#x0345] | [#x0360-#x0361] | [#x0483-#x0486] | [#x0591-#x05A1] | [#x05A3-#x05B9] | [#x05BB-#x05BD] | #x05BF | [#x05C1-#x05C2] | #x05C4 | [#x064B-#x0652] | #x0670 | [#x06D6-#x06DC] | [#x06DD-#x06DF] | [#x06E0-#x06E4] | [#x06E7-#x06E8] | [#x06EA-#x06ED] | [#x0901-#x0903] | #x093C | [#x093E-#x094C] | #x094D | [#x0951-#x0954] | [#x0962-#x0963] | [#x0981-#x0983] | #x09BC | #x09BE | #x09BF | [#x09C0-#x09C4] | [#x09C7-#x09C8] | [#x09CB-#x09CD] | #x09D7 | [#x09E2-#x09E3] | #x0A02 | #x0A3C | #x0A3E | #x0A3F | [#x0A40-#x0A42] | [#x0A47-#x0A48] | [#x0A4B-#x0A4D] | [#x0A70-#x0A71] | [#x0A81-#x0A83] | #x0ABC | [#x0ABE-#x0AC5] | [#x0AC7-#x0AC9] | [#x0ACB-#x0ACD] | [#x0B01-#x0B03] | #x0B3C | [#x0B3E-#x0B43] | [#x0B47-#x0B48] | [#x0B4B-#x0B4D] | [#x0B56-#x0B57] | [#x0B82-#x0B83] | [#x0BBE-#x0BC2] | [#x0BC6-#x0BC8] | [#x0BCA-#x0BCD] | #x0BD7 | [#x0C01-#x0C03] | [#x0C3E-#x0C44] | [#x0C46-#x0C48] | [#x0C4A-#x0C4D] | [#x0C55-#x0C56] | [#x0C82-#x0C83] | [#x0CBE-#x0CC4] | [#x0CC6-#x0CC8] | [#x0CCA-#x0CCD] | [#x0CD5-#x0CD6] | [#x0D02-#x0D03] | [#x0D3E-#x0D43] | [#x0D46-#x0D48] | [#x0D4A-#x0D4D] | #x0D57 | #x0E31 | [#x0E34-#x0E3A] | [#x0E47-#x0E4E] | #x0EB1 | [#x0EB4-#x0EB9] | [#x0EBB-#x0EBC] | [#x0EC8-#x0ECD] | [#x0F18-#x0F19] | #x0F35 | #x0F37 | #x0F39 | #x0F3E | #x0F3F | [#x0F71-#x0F84] | [#x0F86-#x0F8B] | [#x0F90-#x0F95] | #x0F97 | [#x0F99-#x0FAD] | [#x0FB1-#x0FB7] | #x0FB9 | [#x20D0-#x20DC] | #x20E1 | [#x302A-#x302F] | #x3099 | #x309A 
 // Digit ::= [#x0030-#x0039] | [#x0660-#x0669] | [#x06F0-#x06F9] | [#x0966-#x096F] | [#x09E6-#x09EF] | [#x0A66-#x0A6F] | [#x0AE6-#x0AEF] | [#x0B66-#x0B6F] | [#x0BE7-#x0BEF] | [#x0C66-#x0C6F] | [#x0CE6-#x0CEF] | [#x0D66-#x0D6F] | [#x0E50-#x0E59] | [#x0ED0-#x0ED9] | [#x0F20-#x0F29] 
 // Extender ::= #x00B7 | #x02D0 | #x02D1 | #x0387 | #x0640 | #x0E46 | #x0EC6 | #x3005 | [#x3031-#x3035] | [#x309D-#x309E] | [#x30FC-#x30FE] 
-
-N(მთავარი) { print("hey\n"); A2(exports, os_ls) O; }
-static void init() {}
 
 // clang-format off
 EN(tail,
