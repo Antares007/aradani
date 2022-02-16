170848b92bcb616f22e26ca85211760759e66a31
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-02-16 20:25:18 +0400

    multitasking & dynamic loading

--- src/a_epoll.c
+++ src/a_epoll.c
@@ -2,6 +2,7 @@
 #include "gotgod.h"
 // clang-format off
 IBS(                L)IN(L,
+ada,                L)IN(L,
 //
 l_accept,           L)IN(L,
 l_address,          L)IN(L,
@@ -14,7 +15,9 @@ l_read,             L)IN(L,
 l_setnoblock,       L)IN(L,
 l_socket,           L)IN(L,
 //
+os_bark,            L)IN(L,
 os_hrtime,          L)IN(L,
+os_ls,              L)IN(L,
 os_new,             L)IN(L,
 os_queue,           L)IN(L,
 //
@@ -23,7 +26,18 @@ and2,               L)IN(L,
 and3or,             L)IN(L,
 and5,         imports);
 
-S(მთავარი) { C(1); }
+S(მთავარი) {
+  const char* cc1 = "src/a_cycle.arsi";
+  const char* cc2 = "src/a_async.arsi";
+  const char* cc3 = "src/a_parse.arsi";
+  As(
+    god,
+    cc1, exports, ο, os_bark, 040, ada,
+    cc2, exports, ο, os_bark, 040, ada,
+    cc3, exports, ο, os_bark, 040, ada
+  ) O;
+}
+
 
 void init() { print("\ninit %s\n", __FILE__); }
 // clang-format off
