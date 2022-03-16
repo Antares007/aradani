f436ee1a6a7450c46b2dd09e1d7c74cf24a56e61
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-03-16 16:38:14 +0400

    add q_remove

--- src/a_queue.c
+++ src/a_queue.c
@@ -1,6 +1,7 @@
 #include "arsi.h"
 // clang-format off
 IBS(                L)IN(L,
+debugger,           L)IN(L,
 god,                L)IN(L,
 gor,                L)IN(L,
 got,                L)IN(L,
@@ -11,7 +12,8 @@ os_wordump,         L)IN(L,
 and,                L)IN(L,
 and2,               L)IN(L,
 and3,               L)IN(L,
-and4,         imports);
+and4,               L)IN(L,
+andor,        imports);
 // clang-format off
 #include "os/queue.h"
 S(queue_insert_tail) {
@@ -39,13 +41,16 @@ S(roll_in) {
     σ[α + i].v = qσ[i + 3].v;
   α += qα, C(1);
 }
+S(q_remove) {
+  R(QUEUE *, q);
+  QUEUE_REMOVE(q), A2(q, l_free) O;
+}
 S(roll_in_and_free) {
   R(QUEUE *, q);
   R(QUEUE *, h);
   if (q == h)
     return C(0);
-  QUEUE_REMOVE(q);
-  A5(q, roll_in, q, l_free, and2) O;
+  A5(q, roll_in, q, q_remove, and2) O;
 }
 S(q_push) {
   R(QUEUE *, h);
@@ -74,9 +79,8 @@ S(q_for_each_n) {
   if (q == h)
     C(1);
   else
-    A9(q, roll_in, n, and, n, h, QUEUE_NEXT(q), q_for_each_n, and4) O;
+    A10(q, q, roll_in, n, and, n, h, QUEUE_NEXT(q), q_for_each_n, and4) O;
 }
-
 S(q_for_each) {
   R(QUEUE *, h);
   R(n_t, n);
@@ -87,16 +91,21 @@ SarP(init)(god);
 SP(pgod) {
   R(Q_t, a);
   R(Q_t, b);
-  A(a + b) C(1);
+  R(QUEUE*, q);
+  (void)q;
+  //A3(a + b, q, q_remove) O;
+  A2(a + b, god) O;
 }
 SP(Main_n) {
   R(QUEUE *, h);
-  Α(6, 3, h, q_push,
+  Α(debugger, 
+    6, 3, h, q_push, and4,
     3, 6, h, q_push, and4,
     pgod, h, q_for_each, and3,
           h, q_shift, and2,
           h, q_shift, and2,
-          os_wordump, and) O;
+          os_wordump,
+          os_wordump, andor) O;
 }
 SP(Main) {
   QUEUE h;
@@ -107,6 +116,7 @@ EN(tail,
 q_for_each,         L)EN(L,
 q_pop,              L)EN(L,
 q_push,             L)EN(L,
+q_remove,           L)EN(L,
 q_shift,            L)EN(L,
 q_unshift,          L)EN(L,
 Main,         exports)
