509fb0cd3f3bca951c2a394805ddd6f0e8286b36
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-03-17 05:49:47 +0400

    optimize os_queue_n

--- src/os/queue.c
+++ src/os/queue.c
@@ -9,16 +9,21 @@ N(os_queue_n) {
   R(p_t *, nο);
   p_t *nσ;
   Q_t nα = wc;
-  nσ = malloc(0x1000);
-  α -= wc;
-  while (wc)
-    wc--, nσ[wc].v = σ[α + wc].v;
+  if (α == wc)
+    nσ = σ, σ = malloc(0x1000), α = 0;
+  else {
+    nσ = malloc(0x1000);
+    α -= wc;
+    while (wc)
+      wc--, nσ[wc].v = σ[α + wc].v;
+  }
   QUEUE_INSERT_TAIL((QUEUE *)&ο[Ψ], (QUEUE *)&nσ[nα + 0]);
   nσ[nα + 2].Q = nα;
   nσ[nα + 3].v = nο;
   nσ[nα + 4].Q = nο[Ρ].Q;
   C(1);
 }
+
 N(os_queue) {
   Q_t a = α - 1;
   A2(a, os_queue_n) O;
