6b128d871192982f49e4b75efb3db078e7321864
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-03-22 07:33:50 +0400

    use Nargos Sargos Sos and Nos

--- src/a_stream.c
+++ src/a_stream.c
@@ -22,66 +22,66 @@ epoll_ctl_add_in,   L)IN(L,
 epoll_ctl_del_in,   imports)
 
 
-SarP(init)(god)
+Sargo(init)(god)
 
-S(reject ) { R(p_t*, rο); R(p_t*, lο); Α(lο, got, rο, 2, os_queue_n) O; }
-S(forward) { R(p_t*, oο);              Α(    god, oο,    os_queue  ) O; }
-S(greet  ) { R(p_t*, rο); R(p_t*, lο); Α(rο, gor, lο, 2, os_queue_n) O; }
-S(greet_b) { R(p_t*, rο); R(p_t*, lο); Α(lο, gor, rο, 2, os_queue_n) O; }
+Sarg2(reject )(lο,p_t*, rο,p_t*)(lο, got, rο, 2, os_queue_n)
+Sarg2(greet  )(lο,p_t*, rο,p_t*)(rο, gor, lο, 2, os_queue_n)
+Sarg2(greet_b)(lο,p_t*, rο,p_t*)(lο, gor, rο, 2, os_queue_n)
+Sarg1(forward)(oο,p_t*         )(god, oο, os_queue)
 
-S(set_alfa_zero) { α = 0, C(1); } 
+So(set_alfa_zero)( α = 0, C(1); )
 
-Sar(new_soll_not)(ο[ 8].p, os_unsoll, dot, and)
-Sar(new_soll_and)(ο[ 9].p, os_unsoll, dot, and)
-Sar(new_soll_oor)(ο[10].p, os_unsoll, dot, and)
-S(new_soll_set) {
-  R(p_t *, not_soll);
-  R(p_t *, and_soll);
-  R(p_t *, oor_soll);
-  R(p_t *, set_soll);
-  R(p_t *, oο);
+Sargo(new_soll_not)(ο[ 8].p, os_unsoll, dot, and)
+Sargo(new_soll_and)(ο[ 9].p, os_unsoll, dot, and)
+Sargo(new_soll_oor)(ο[10].p, os_unsoll, dot, and)
+N5(new_soll_set)(oο,        p_t*,
+                 set_soll,  p_t*,
+                 oor_soll,  p_t*,
+                 and_soll,  p_t*,
+                 not_soll,  p_t*)(
   oο[ 7].p = set_soll;
   oο[ 8].p = not_soll;
   oο[ 9].p = and_soll;
   oο[10].p = oor_soll;
-  A(oο) C(1);
-}
-Narg7(new_soll_psn)(not_soll,p_t*, and_soll,p_t*, oor_soll,p_t*, set_soll,p_t*, oο,p_t*, size,Q_t, name,const char*)(
+  A(oο) C(1);)
+Sarg7(new_soll_psn)(not_soll, p_t*,
+                    and_soll, p_t*,
+                    oor_soll, p_t*,
+                    set_soll, p_t*,
+                    oο,       p_t*,
+                    size,     Q_t,
+                    name,     const char*)(
   new_soll_not, new_soll_and, new_soll_oor, oο, size, name, os_new_psn,
   set_soll, oor_soll, and_soll, not_soll, new_soll_set, and5)
 
-Sar(new_soll_psn_a)(new_soll_psn, and4)
+Sargo(new_soll_psn_a)(new_soll_psn, and4)
 
-S(tos) { print("-> %lu\n", ο[7].p[4]); C(1); }
-S(s_readable) {
-  R(p_t *, set_soll);
-  R(p_t *, destroy_soll);
-  R(p_t *, read_soll);
-  R(p_t *, construct_soll);
-  Α(tos, tos, and, tos, and, 5, os_soll_n,
-    tos, tos, and, god, and, 5, os_soll_a,
-    tos, god, and, god, and, 5, os_soll_a,
-    set_soll, construct_soll, read_soll, destroy_soll, 4, os_soll_a,
-    ο, 512, "Š", new_soll_psn_a) O;
-}
-Nar(s_stdin)(
+So(tos)( print("-> %lu\n", ο[7].p[4]); C(1); )
+Sarg4(s_readable)(construct_soll, p_t*,
+                  read_soll,      p_t*,
+                  destroy_soll,   p_t*,
+                  set_soll,       p_t*)(
+  tos, tos, and, tos, and, 5, os_soll_n,
+  tos, tos, and, god, and, 5, os_soll_a,
+  tos, god, and, god, and, 5, os_soll_a,
+  set_soll, construct_soll, read_soll, destroy_soll, 4, os_soll_a,
+  ο, 512, "Š", new_soll_psn_a)
+Nargo(s_stdin)(
   epoll_ctl_del_in, 1, os_soll_n,
   tos, tos, and, god, and, 5, os_soll_a,
   epoll_ctl_add_in, 1, os_soll_a,
   1,   2,   3,   4,   5,   5, os_soll_a,
   s_readable, and)
 
-Nar(s_writeable)(os_wordump, set_alfa_zero, and)
-Sar(nsoll)(
+Nargo(s_writeable)(os_wordump, set_alfa_zero, and)
+Sargo(nsoll)(
   tos, tos, and, tos, and, 5, os_soll_n,
   tos, tos, and, god, and, 5, os_soll_a,
   tos, god, and, god, and, 5, os_soll_a,
   1,   2,   3,   4,   5,   5, os_soll_a,
   ο, 512, "Š", new_soll_psn_a)
 
-Sar(example)(
-  god, nsoll, os_queue, and
-)
+Sargo(example)(god, nsoll, os_queue, and)
 
 /*
   readable._construct(callback)  Call this function (optionally with an error argument) when the stream has finished initializing.
--- src/oars_nargos.h
+++ src/oars_nargos.h
@@ -1,70 +1,88 @@
 #pragma once
+#define Nargo_s(...) Α(__VA_ARGS__) O; }
+#define N_s(...) {__VA_ARGS__} }
+#define Nargo(Name) Narg0(Name)()
+#define No(Name) N0(Name)()
+#define Sargo(Name) static Nargo(Name)
+#define So(Name) static No(Name) // :)
 /*
-const rs_Nargo_r = (a, i) =>
+const RS = (a, i) =>
   a
     .slice(0, i)
     .reverse()
     .map((c) => `R(T${c}, P${c});`)
     .join("");
-const args_Nargo_r = (a, i) =>
+const Args = (a, i) =>
   a
     .slice(0, i)
     .map((c) => `P${c},T${c}`)
     .join(", ");
 const define_Nargo_r = (v, i, a) =>
-  `#define Nargo_r${i}(${args_Nargo_r(a, i)}) { ${rs_Nargo_r(a, i)} Nargo_s`;
+  `#define Nargo_r${i}(${Args(a, i)}) { ${RS(a, i)} Nargo_s`;
 
 const define_Narg = (v, i, a) => `#define Narg${i}(Name) N(Name) Nargo_r${i}`;
-console.log(
-  Array(7 + 7 + 7 + 1)
-    .fill()
-    .map((_, a) => String.fromCharCode(a + 0x61))
-    .map((v, i, a) => define_Nargo_r(v, i, a) + "\n" + define_Narg(v, i, a))
-    .join("\n")
-);
-*/
-#define Nargo_s(...) Α(__VA_ARGS__) O; }
+
+const define_N_r = (v, i, a) =>
+  `#define N_r${i}(${Args(a, i)}) { ${RS(a, i)} N_s`;
+const define_N = (v, i, a) => `#define N${i}(Name) N(Name) N_r${i}`;
+
+const define_Sarg = (v, i, a) => `#define Sarg${i}(Name) static Narg${i}(Name)`;
+const define_S = (v, i, a) => `#define S${i}(Name) static N${i}(Name)`;
+
+const alfa = Array(7 + 1)
+  .fill()
+  .map((_, a) => String.fromCharCode(a + 0x61));
+console.log(alfa.map(define_Nargo_r).join("\n"));
+console.log(alfa.map(define_Narg).join("\n"));
+console.log(alfa.map(define_N_r).join("\n"));
+console.log(alfa.map(define_N).join("\n"));
+console.log(alfa.map(define_Sarg).join("\n"));
+console.log(alfa.map(define_S).join("\n"));*/
 #define Nargo_r0() {  Nargo_s
-#define Narg0(Name) N(Name) Nargo_r0
 #define Nargo_r1(Pa,Ta) { R(Ta, Pa); Nargo_s
-#define Narg1(Name) N(Name) Nargo_r1
 #define Nargo_r2(Pa,Ta, Pb,Tb) { R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg2(Name) N(Name) Nargo_r2
 #define Nargo_r3(Pa,Ta, Pb,Tb, Pc,Tc) { R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg3(Name) N(Name) Nargo_r3
 #define Nargo_r4(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td) { R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg4(Name) N(Name) Nargo_r4
 #define Nargo_r5(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te) { R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg5(Name) N(Name) Nargo_r5
 #define Nargo_r6(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf) { R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg6(Name) N(Name) Nargo_r6
 #define Nargo_r7(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf, Pg,Tg) { R(Tg, Pg);R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
+#define Narg0(Name) N(Name) Nargo_r0
+#define Narg1(Name) N(Name) Nargo_r1
+#define Narg2(Name) N(Name) Nargo_r2
+#define Narg3(Name) N(Name) Nargo_r3
+#define Narg4(Name) N(Name) Nargo_r4
+#define Narg5(Name) N(Name) Nargo_r5
+#define Narg6(Name) N(Name) Nargo_r6
 #define Narg7(Name) N(Name) Nargo_r7
-#define Nargo_r8(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf, Pg,Tg, Ph,Th) { R(Th, Ph);R(Tg, Pg);R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg8(Name) N(Name) Nargo_r8
-#define Nargo_r9(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf, Pg,Tg, Ph,Th, Pi,Ti) { R(Ti, Pi);R(Th, Ph);R(Tg, Pg);R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg9(Name) N(Name) Nargo_r9
-#define Nargo_r10(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf, Pg,Tg, Ph,Th, Pi,Ti, Pj,Tj) { R(Tj, Pj);R(Ti, Pi);R(Th, Ph);R(Tg, Pg);R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg10(Name) N(Name) Nargo_r10
-#define Nargo_r11(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf, Pg,Tg, Ph,Th, Pi,Ti, Pj,Tj, Pk,Tk) { R(Tk, Pk);R(Tj, Pj);R(Ti, Pi);R(Th, Ph);R(Tg, Pg);R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg11(Name) N(Name) Nargo_r11
-#define Nargo_r12(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf, Pg,Tg, Ph,Th, Pi,Ti, Pj,Tj, Pk,Tk, Pl,Tl) { R(Tl, Pl);R(Tk, Pk);R(Tj, Pj);R(Ti, Pi);R(Th, Ph);R(Tg, Pg);R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg12(Name) N(Name) Nargo_r12
-#define Nargo_r13(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf, Pg,Tg, Ph,Th, Pi,Ti, Pj,Tj, Pk,Tk, Pl,Tl, Pm,Tm) { R(Tm, Pm);R(Tl, Pl);R(Tk, Pk);R(Tj, Pj);R(Ti, Pi);R(Th, Ph);R(Tg, Pg);R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg13(Name) N(Name) Nargo_r13
-#define Nargo_r14(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf, Pg,Tg, Ph,Th, Pi,Ti, Pj,Tj, Pk,Tk, Pl,Tl, Pm,Tm, Pn,Tn) { R(Tn, Pn);R(Tm, Pm);R(Tl, Pl);R(Tk, Pk);R(Tj, Pj);R(Ti, Pi);R(Th, Ph);R(Tg, Pg);R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg14(Name) N(Name) Nargo_r14
-#define Nargo_r15(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf, Pg,Tg, Ph,Th, Pi,Ti, Pj,Tj, Pk,Tk, Pl,Tl, Pm,Tm, Pn,Tn, Po,To) { R(To, Po);R(Tn, Pn);R(Tm, Pm);R(Tl, Pl);R(Tk, Pk);R(Tj, Pj);R(Ti, Pi);R(Th, Ph);R(Tg, Pg);R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg15(Name) N(Name) Nargo_r15
-#define Nargo_r16(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf, Pg,Tg, Ph,Th, Pi,Ti, Pj,Tj, Pk,Tk, Pl,Tl, Pm,Tm, Pn,Tn, Po,To, Pp,Tp) { R(Tp, Pp);R(To, Po);R(Tn, Pn);R(Tm, Pm);R(Tl, Pl);R(Tk, Pk);R(Tj, Pj);R(Ti, Pi);R(Th, Ph);R(Tg, Pg);R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg16(Name) N(Name) Nargo_r16
-#define Nargo_r17(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf, Pg,Tg, Ph,Th, Pi,Ti, Pj,Tj, Pk,Tk, Pl,Tl, Pm,Tm, Pn,Tn, Po,To, Pp,Tp, Pq,Tq) { R(Tq, Pq);R(Tp, Pp);R(To, Po);R(Tn, Pn);R(Tm, Pm);R(Tl, Pl);R(Tk, Pk);R(Tj, Pj);R(Ti, Pi);R(Th, Ph);R(Tg, Pg);R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg17(Name) N(Name) Nargo_r17
-#define Nargo_r18(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf, Pg,Tg, Ph,Th, Pi,Ti, Pj,Tj, Pk,Tk, Pl,Tl, Pm,Tm, Pn,Tn, Po,To, Pp,Tp, Pq,Tq, Pr,Tr) { R(Tr, Pr);R(Tq, Pq);R(Tp, Pp);R(To, Po);R(Tn, Pn);R(Tm, Pm);R(Tl, Pl);R(Tk, Pk);R(Tj, Pj);R(Ti, Pi);R(Th, Ph);R(Tg, Pg);R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg18(Name) N(Name) Nargo_r18
-#define Nargo_r19(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf, Pg,Tg, Ph,Th, Pi,Ti, Pj,Tj, Pk,Tk, Pl,Tl, Pm,Tm, Pn,Tn, Po,To, Pp,Tp, Pq,Tq, Pr,Tr, Ps,Ts) { R(Ts, Ps);R(Tr, Pr);R(Tq, Pq);R(Tp, Pp);R(To, Po);R(Tn, Pn);R(Tm, Pm);R(Tl, Pl);R(Tk, Pk);R(Tj, Pj);R(Ti, Pi);R(Th, Ph);R(Tg, Pg);R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg19(Name) N(Name) Nargo_r19
-#define Nargo_r20(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf, Pg,Tg, Ph,Th, Pi,Ti, Pj,Tj, Pk,Tk, Pl,Tl, Pm,Tm, Pn,Tn, Po,To, Pp,Tp, Pq,Tq, Pr,Tr, Ps,Ts, Pt,Tt) { R(Tt, Pt);R(Ts, Ps);R(Tr, Pr);R(Tq, Pq);R(Tp, Pp);R(To, Po);R(Tn, Pn);R(Tm, Pm);R(Tl, Pl);R(Tk, Pk);R(Tj, Pj);R(Ti, Pi);R(Th, Ph);R(Tg, Pg);R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg20(Name) N(Name) Nargo_r20
-#define Nargo_r21(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf, Pg,Tg, Ph,Th, Pi,Ti, Pj,Tj, Pk,Tk, Pl,Tl, Pm,Tm, Pn,Tn, Po,To, Pp,Tp, Pq,Tq, Pr,Tr, Ps,Ts, Pt,Tt, Pu,Tu) { R(Tu, Pu);R(Tt, Pt);R(Ts, Ps);R(Tr, Pr);R(Tq, Pq);R(Tp, Pp);R(To, Po);R(Tn, Pn);R(Tm, Pm);R(Tl, Pl);R(Tk, Pk);R(Tj, Pj);R(Ti, Pi);R(Th, Ph);R(Tg, Pg);R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); Nargo_s
-#define Narg21(Name) N(Name) Nargo_r21
+#define N_r0() {  N_s
+#define N_r1(Pa,Ta) { R(Ta, Pa); N_s
+#define N_r2(Pa,Ta, Pb,Tb) { R(Tb, Pb);R(Ta, Pa); N_s
+#define N_r3(Pa,Ta, Pb,Tb, Pc,Tc) { R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); N_s
+#define N_r4(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td) { R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); N_s
+#define N_r5(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te) { R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); N_s
+#define N_r6(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf) { R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); N_s
+#define N_r7(Pa,Ta, Pb,Tb, Pc,Tc, Pd,Td, Pe,Te, Pf,Tf, Pg,Tg) { R(Tg, Pg);R(Tf, Pf);R(Te, Pe);R(Td, Pd);R(Tc, Pc);R(Tb, Pb);R(Ta, Pa); N_s
+#define N0(Name) N(Name) N_r0
+#define N1(Name) N(Name) N_r1
+#define N2(Name) N(Name) N_r2
+#define N3(Name) N(Name) N_r3
+#define N4(Name) N(Name) N_r4
+#define N5(Name) N(Name) N_r5
+#define N6(Name) N(Name) N_r6
+#define N7(Name) N(Name) N_r7
+#define Sarg0(Name) static Narg0(Name)
+#define Sarg1(Name) static Narg1(Name)
+#define Sarg2(Name) static Narg2(Name)
+#define Sarg3(Name) static Narg3(Name)
+#define Sarg4(Name) static Narg4(Name)
+#define Sarg5(Name) static Narg5(Name)
+#define Sarg6(Name) static Narg6(Name)
+#define Sarg7(Name) static Narg7(Name)
+#define S0(Name) static N0(Name)
+#define S1(Name) static N1(Name)
+#define S2(Name) static N2(Name)
+#define S3(Name) static N3(Name)
+#define S4(Name) static N4(Name)
+#define S5(Name) static N5(Name)
+#define S6(Name) static N6(Name)
+#define S7(Name) static N7(Name)
