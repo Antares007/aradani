5eee48d06f8ea9abcc2f95bf40a3f488a03baba8
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-03-22 12:25:23 +0400

    add PLog

--- src/oars_np.h
+++ src/oars_np.h
@@ -8,12 +8,14 @@
     n##p(T());                                                                 \
   }                                                                            \
   N(n##p)
+#define PLog_(PRN) PRN("%p %s % 3ld % 3ld %s:%s\n", σ, ο[Ν].cs, α, ρ, __FUNCTION__, __FILE__)
 #ifndef ARSI
 #include <stdio.h>
+#define PLog PLog_(printf)
 #define NP(n) NP_(n, printf, N)
 #define SP(n) NP_(n, printf, S)
 #else
+#define PLog PLog_(print)
 #define NP(n) NP_(n, print, N)
 #define SP(n) NP_(n, print, S)
 #endif
-#undef PRN
