5dec6410d23744ecc6c5b78c95cc8631b31704c1
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-03-29 09:10:00 +0400

    sync parsing

--- src/a_parser_pith.c
+++ src/a_parser_pith.c
@@ -34,99 +34,90 @@ static NP(init) { C(1); }
 N(pgot) { Α(got) O; }
 N(pgod) { Α(god) O; }
 N(pgor) { Α(gor) O; }
-N(spith);
+N(new_pith);
 
 #define LOG print("%lx %2lu α:%lu σ[α-1].Q:%lu %s\n", ((Q_t)σ >> 12) & 0xff, ο[3].Q, α, σ[α-1].Q, __FUNCTION__)
-N(queue) { os_queue(T()); }
-N(queue_n) { os_queue_n(T()); }
-N(queue_soll) { os_queue_soll(T()); }
-
-N(empty);
-N(empty_oor) {
-  LOG;
-  R(Q_t, pos);
-  R(Q_t, len);
-  R(void *, buf);
-  R(p_t *, cο);
-  Α(buf, len, pos, gor, cο, queue) O;
+#define CοBufLenPos LOG; R(Q_t, pos); R(Q_t, len); R(const char *, buf); R(p_t *, cο)
+N(Q)  { os_queue(T()); }
+N(QN) { os_queue_n(T()); }
+N(QS) { os_queue_soll(T()); }
+N(sοpop) {
+  R(p_t*, sο);
+  if (sο[-1].Q)
+    A(sο[--sο[-1].Q].v) C(1); 
+  else C(2);
 }
-N(empty) { Α(empty_oor, 0, spith) O; }
-
-N(orelse);
+N(sοpush) {
+  R(p_t*, sο);
+  R(void*, v);
+  if ((sο[-1].Q + 1) < sο[-2].Q)
+    sο[sο[-1].Q++].v = v, A(sο) C(1); 
+  else C(2);
+}
+N(vε_oor) {
+  CοBufLenPos;
+  Α(cο, buf, len, pos, gor, cο, sοpop,
+    5, QN, and2) O;
+}
 N(orelse_oor) {
-  LOG;
-  Α(gor,
-    os_soll,
+  CοBufLenPos;
+  Α(cο, buf, len, pos, gor, 5, os_soll_n,
     os_soll_dup, and,
-    ο[7].p, queue_soll, and2,
-    ο[8].p, queue_soll, and2) O;
+    ο[7].p, QS, and2,
+    ο[8].p, QS, and2) O;
 }
-N(orelse) { Α(orelse_oor, 2, spith) O; }
-
-N(thenS);
 N(thenS_oor) {
-  LOG;
-  R(Q_t, pos);
-  R(Q_t, len);
-  R(void *, buf);
-  Α(ο[8].p, buf, len, pos, gor, ο[7].p, queue) O;
+  CοBufLenPos;
+  Α(ο[8].p, cο, sοpush,
+    buf, len, pos, gor, ο[7].p, 5, QN, and7) O;
 }
-N(thenS) { Α(thenS_oor, 2, spith) O; }
-
-N(term);
 N(term_oor) {
-  LOG;
-  R(Q_t, pos);
-  R(Q_t, len);
-  R(const char *, buf);
-  R(p_t *, cο);
+  CοBufLenPos;
   if (pos >= len) {
-    print("A\n");
-    α = 0, C(1);
+    print("A\n"), C(1);
   } else if (ο[7].Q == (Q_t)buf[pos]) {
-    print("B\n"), Α(buf, len, pos + 1, gor, cο, queue) O;
+    print("B\n"), Α(cο, buf, len, pos + 1, gor, cο, sοpop,
+                    5, QN, and2) O;
   } else {
-    print("C\n");
-    α = 0, C(1);
+    print("C\n"), C(1);
   }
 }
-N(term) { Α(term_oor, 1, spith) O; }
+N(var_oor) {
+  CοBufLenPos;
+  Α(cο, buf, len, pos, gor, ο[7].p, 5, QN) O;
+}
+N(orelse) { Α(orelse_oor, 2, new_pith) O; }
+N(thenS) { Α(thenS_oor, 2, new_pith) O; }
+N(term) { Α(term_oor, 1, new_pith) O; }
+N(var) { Α(var_oor, 1, new_pith) O; }
+N(vε) { Α(vε_oor, 0, new_pith) O; }
 
 N(term_s) { Α('s', term) O; }
 N(term_a) { Α('a', term) O; }
 N(Ο)      { Α(ο) C(1); }
 
-N(sen) { Α(term_s, ο, thenS, and2, ο, thenS, and2) O; }
-N(sS_oor) {
-  LOG;
-  Α(
-  gor,
-    empty, 
-      sen, and, orelse, and,
-  queue, and) O;
-}// term_s, term_a, and, thenS,  and, term_s, and, thenS,  and,
-N(sS) { Α(sS_oor, 0, spith) O; }
-
+N(sS1) { Α(term_s, ο, thenS, and2, ο, thenS, and2) O; }
+N(sS2) { Α(vε) O; }
+N(sS) { Α(sS1,
+          sS2, and, orelse, and) O; }
 NP(exam) {
-        Α(ο, "sasss", 5, 0, gor,
-            //sS,
-            term_a,
-            queue, and) O;
+        Α(ο, 1, os_soll_n,
+          "sssss", 5, 0, gor, sS, and5,
+          Q, and) O;
 }
+
 N(drop3) {
-  if (α == 3) {
-    R(Q_t, pos);
-    R(Q_t, len);
-    R(const char *, buf);
-    print("\n---> %s(%lu)%lu\n", buf,pos,len);
+  if (α == 4) {
+    CοBufLenPos;
+    print(">%s(%lu)%lu - %lu\n", buf,pos,len, cο[-1].Q);
     C(1);
   } else Α(os_wordump) O;
 }
-NP(Not) { Α(drop3) O; }
-NP(And) { Α(drop3) O; }
-NP(Oor) { Α(drop3) O; }
+N(Not) { print("N\n"), Α(drop3) O; }
+N(And) { print("A\n"), Α(drop3) O; }
+N(Oor) { print("O\n"), Α(drop3) O; }
 N(mk_dumper) { Α(ο, 0, Oor, And, Not, 512, os_new_pith) O; }
-N(მთავარი) { Α(exam, mk_dumper, 1, queue_n, and2) O; }
+N(მთავარი) { Α(exam, mk_dumper, 1, QN, and2) O; }
 
 // clang-format off
 EN(tail,
@@ -141,7 +132,7 @@ N(set_state) {
   Α(oο) C(1);
 }
 
-N(spith) {
+N(new_pith) {
   R(Q_t, wc);
   R(n_t, oor);
   Α(ο[Φ].p, 0, oor, god, got, 512, os_new_pith, wc, set_state, and2) O;
