bae6af9bbdd136f5bebd2f82bfb6c82c240713ea
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-04-22 10:55:42 +0400

    use power of the word to express and specify

--- src/a_bat3.c
+++ src/a_bat3.c
@@ -50,11 +50,7 @@ or2,                L)IN(L,
 or3,                L)IN(L,
 or4,                L)IN(L,
 or5,          imports);
-typedef struct lp_t { const char* input; Q_t len; Q_t pos; p_t* continuation;  p_t* solls; } lp_t;
-#define TS(T) T*o=(T*)ο;(void)o
-#define Var_(...) {TS(lp_t);Α(__VA_ARGS__) O;}
-#define Var(Name) N(Name) Var_
-#define VarP(Name) NP(Name) Var_
+
 N(soll_pop) {
   R(p_t *, sο);
   if (sο[Ǎ].Q)
@@ -78,7 +74,9 @@ N(soll_contains) {
   C(0);
 }
 
-//static N(ps    ){ R(const char*, str); print("%s", str), C(1); }
+N(set_alfa_zero ) { α = 0, C(1); }    
+
+static N(ps    ){ R(const char*, str); print("%s", str), C(1); }
 //static N(pld   ){ R(q_t, v); print("%ld", v), C(1); }
 //static N(pnl   ){                      print("\n"     ), C(1); }
 //static N(plu   ){ R(Q_t, v); print("%lu", v), C(1); }
@@ -88,42 +86,33 @@ N(drop          ){ α--, C(1); }
 N(empty_soll    ){ Α(0, os_soll_n) O; }
 N(empty_pith    ){ Α(empty_soll) O; }
 
-NP(term         ){ TS(lp_t);
-  R(const char*,  str);
-  print("input:%s len:%lu pos:%lu str:%s\n", o->input, o->len, o->pos, str);
-  if (o->pos < o->len && o->input[o->pos] == str[0])
-    print("ok pos:%lu\n", o->pos + 1), C(1);
-  else
-    print("drop/unsoll? pos:%lu\n", o->pos), C(1);
-}
-N(extend_pith   ){ Α(ο) C(1); }
-NP(orelse       ){
-  Α(extend_pith, coll, and,
-    dot, and) O;
-}
-N(is_left_rec_condition) { TS(lp_t); Α(o->solls, soll_contains) O; }
+typedef struct lp_t { const char* input; Q_t len; Q_t pos; p_t* continuation;  p_t* solls; } lp_t;
+#define TS(T) T*o=(T*)ο;(void)o
+#define Var_(...) {TS(lp_t);Α(__VA_ARGS__) O;}
+#define Var(Name) N(Name) Var_
+#define VarP(Name) NP(Name) Var_
 
-NP(thenS        ){ TS(lp_t);
-  Α(drop, is_left_rec_condition, and,
-     drop,
-     dot, andor) O;
-}
-N(set_in_entered_vars_set) { TS(lp_t);
-  Α(o->solls, soll_contains, drop, o->solls, soll_push, andor2) O;
-}
-NP(var         ){ TS(lp_t);
-  Α(set_in_entered_vars_set, dot, and) O; }
+Var(is_left_rec_condition  )(o->solls, soll_contains)
+Var(set_in_entered_vars_set)(o->solls, soll_contains, drop, o->solls, soll_push, andor2)
+N(is_match){ TS(lp_t); R(const char*,  str); C(o->pos < o->len && o->input[o->pos] == str[0]); }
 
-Nar(pls        )("+", term)
-Nar(mns        )("-", term)
-Nar(mul        )("*", term)
-Nar(div        )("/", term)
-Nar(id         )("a", term)
-Nar(opr        )("(", term)
-Nar(cpr        )(")", term)
-Nar(cma        )(",", term)
+Var (extend_pith  )(ο, god)
+
+VarP(term         )(is_match, "yes", ps, "no", ps, and2or2)
+VarP(orelse       )(extend_pith, coll, and, dot, and)
+VarP(thenS        )(drop, is_left_rec_condition, and, drop, dot, andor)
+VarP(var          )(set_in_entered_vars_set, dot, and)
+
+Var(pls           )("+", term)
+Var(mns           )("-", term)
+Var(mul           )("*", term)
+Var(div           )("/", term)
+Var(id            )("a", term)
+Var(opr           )("(", term)
+Var(cpr           )(")", term)
+Var(cma           )(",", term)
 // id, id, id, orelse, thenS3
-Nar(Exp        )(
+Var(Exp        )(
   id,
   opr, Exp, thenS, cpr, thenS, orelse,
   Exp, pls, thenS, Exp, thenS, orelse,
@@ -131,18 +120,17 @@ Nar(Exp        )(
   Exp, div, thenS, Exp, thenS, orelse,
   Exp, mul, thenS, Exp, thenS, orelse,
   Exp, var      )
-N(set_alfa_zero ) { α = 0, C(1); }    
 
 // continuation is chained solls and representing long sentence 
-NP(example) { Α(Exp,
-                 "(a+a)*a", 7, 0,
-                 os_wordump, set_alfa_zero, and, 3, os_soll_n,
-                 0, os_soll_n, and2,
-                 5, os_soll_n, and2,
-                     os_queue, and) O; }
+Nar(example)(Exp,
+             "(a+a)*a", 7, 0,
+             os_wordump, set_alfa_zero, and, 3, os_soll_n,
+             0, os_soll_n, and2,
+             5, os_soll_n, and2,
+             os_queue, and)
 
-N(მთავარი       ){ Α(example) O; }
-N(init          ){ C(1); }
+Nar(მთავარი)(example)
+Nar(init   )(god)
 
 // clang-format off
 EN(tail,
