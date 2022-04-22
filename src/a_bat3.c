6c57a5fa15be1c7ca3e72be476a939c217c4e45e
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-04-22 10:30:02 +0400

    use language to specify things

--- src/a_bat3.c
+++ src/a_bat3.c
@@ -42,6 +42,7 @@ and5or4,            L)IN(L,
 and6or4,            L)IN(L,
 and7or,             L)IN(L,
 andor,              L)IN(L,
+andor2,             L)IN(L,
 andor4,             L)IN(L,
 andor5,             L)IN(L,
 or,                 L)IN(L,
@@ -49,78 +50,96 @@ or2,                L)IN(L,
 or3,                L)IN(L,
 or4,                L)IN(L,
 or5,          imports);
-typedef struct lp_t { n_t c; const char* input; Q_t len; Q_t pos; } lp_t;
+typedef struct lp_t { const char* input; Q_t len; Q_t pos; p_t* continuation;  p_t* solls; } lp_t;
 #define TS(T) T*o=(T*)ο;(void)o
 #define Var_(...) {TS(lp_t);Α(__VA_ARGS__) O;}
 #define Var(Name) N(Name) Var_
 #define VarP(Name) NP(Name) Var_
+N(soll_pop) {
+  R(p_t *, sο);
+  if (sο[Ǎ].Q)
+    A(sο[--sο[Ǎ].Q].v) C(1);
+  else
+    C(0);
+}
+N(soll_push) {
+  R(p_t *, sο);
+  R(void *, v);
+  if ((sο[Ǎ].Q + 1) < sο[Σ].Q)
+    sο[sο[Ǎ].Q++].v = v, C(1);
+  else
+    C(0);
+}
+N(soll_contains) {
+  R(p_t *, sο);
+  for (Q_t i = 0; i < sο[Ǎ].Q; i++)
+    if (sο[i].v == σ[α - 1].v)
+      return C(1);
+  C(0);
+}
 
 //static N(ps    ){ R(const char*, str); print("%s", str), C(1); }
 //static N(pld   ){ R(q_t, v); print("%ld", v), C(1); }
 //static N(pnl   ){                      print("\n"     ), C(1); }
 //static N(plu   ){ R(Q_t, v); print("%lu", v), C(1); }
+//
 N(coll          ){ R(p_t*, oο); R(n_t, nar); nar(oο, α, ρ, σ); }
 N(drop          ){ α--, C(1); }
 N(empty_soll    ){ Α(0, os_soll_n) O; }
 N(empty_pith    ){ Α(empty_soll) O; }
 
-NP(term         ){
+NP(term         ){ TS(lp_t);
   R(const char*,  str);
-  R(Q_t,          pos);
-  R(Q_t,          len);
-  R(const char*,  input);
-  print("input:%s len:%lu pos:%lu str:%s\n", input, len, pos, str);
-  if (pos < len && input[pos] == str[0])
-    Α(input, len, pos + 1, ο, os_unsoll_free, dot, and) O;
+  print("input:%s len:%lu pos:%lu str:%s\n", o->input, o->len, o->pos, str);
+  if (o->pos < o->len && o->input[o->pos] == str[0])
+    print("ok pos:%lu\n", o->pos + 1), C(1);
   else
-    print("drop/unsoll? pos:%lu\n", pos), C(1);
+    print("drop/unsoll? pos:%lu\n", o->pos), C(1);
 }
-N(extend_c_pith) {
-  Α(ο, os_unsoll, dot, and, 4, os_soll_n) O;
+N(extend_pith   ){ Α(ο) C(1); }
+NP(orelse       ){
+  Α(extend_pith, coll, and,
+    dot, and) O;
 }
-N(orelse_n_n    ){
-  R(p_t*, rhsoll);
-  const char* input = σ[0].cs;
-  Q_t len           = σ[1].Q;
-  Q_t pos           = σ[2].Q;
-  Α(input, len, pos, rhsoll, os_unsoll_free, dot, and,
-                      extend_c_pith,
-                      7, os_queue_n, and2) O;
-}
-NP(orelse_n     ){
-  R(Q_t, wc);
-  Α(wc, os_soll_n, orelse_n_n, and,
-                          dot, and) O;
-}
-NP(thenS_n      ){
-  R(Q_t, wc);
-  Α(ο, os_unsoll, and2, dot, and, wc + 5, os_soll_n, coll, and) O;
-}
-NP(var          ){ Α(drop, dot, and) O; }
+N(is_left_rec_condition) { TS(lp_t); Α(o->solls, soll_contains) O; }
 
-N(thenS         ){ Α(1, thenS_n) O; }
-N(orelse3       ){ Α(3, orelse_n) O; }
-N(orelse5       ){ Α(5, orelse_n) O; }
+NP(thenS        ){ TS(lp_t);
+  Α(drop, is_left_rec_condition, and,
+     drop,
+     dot, andor) O;
+}
+N(set_in_entered_vars_set) { TS(lp_t);
+  Α(o->solls, soll_contains, drop, o->solls, soll_push, andor2) O;
+}
+NP(var         ){ TS(lp_t);
+  Α(set_in_entered_vars_set, dot, and) O; }
 
-NarP(pls        )("+", term)
-NarP(mns        )("-", term)
-NarP(mul        )("*", term)
-NarP(div        )("/", term)
-NarP(id         )("a", term)
-NarP(opr        )("(", term)
-NarP(cpr        )(")", term)
-NarP(cma        )(",", term)
-NarP(Exp        )(
+Nar(pls        )("+", term)
+Nar(mns        )("-", term)
+Nar(mul        )("*", term)
+Nar(div        )("/", term)
+Nar(id         )("a", term)
+Nar(opr        )("(", term)
+Nar(cpr        )(")", term)
+Nar(cma        )(",", term)
+// id, id, id, orelse, thenS3
+Nar(Exp        )(
   id,
-  opr, Exp, thenS, cpr, thenS, orelse5,
-  //Exp, pls, thenS, Exp, thenS, orelse5,
-  //Exp, mns, thenS, Exp, thenS, orelse5,
-  //Exp, div, thenS, Exp, thenS, orelse5,
-  //Exp, mul, thenS, Exp, thenS, orelse5,
+  opr, Exp, thenS, cpr, thenS, orelse,
+  Exp, pls, thenS, Exp, thenS, orelse,
+  Exp, mns, thenS, Exp, thenS, orelse,
+  Exp, div, thenS, Exp, thenS, orelse,
+  Exp, mul, thenS, Exp, thenS, orelse,
   Exp, var      )
-N(set_alfa_zero) { α = 0, C(1); }    
-NP(continuation ) { Α(os_wordump, set_alfa_zero, and, 3, os_soll_n) O; }
-NP(example      ) { Α("(a)+a*a", 7, 0, Exp, continuation, os_queue, and) O; }
+N(set_alfa_zero ) { α = 0, C(1); }    
+
+// continuation is chained solls and representing long sentence 
+NP(example) { Α(Exp,
+                 "(a+a)*a", 7, 0,
+                 os_wordump, set_alfa_zero, and, 3, os_soll_n,
+                 0, os_soll_n, and2,
+                 5, os_soll_n, and2,
+                     os_queue, and) O; }
 
 N(მთავარი       ){ Α(example) O; }
 N(init          ){ C(1); }
