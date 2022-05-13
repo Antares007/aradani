14b92ff5216411be076129b62bbf8cc4a2e21192
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-05-13 08:39:47 +0400

    remove trash from tavtikha

--- src/a_bat5.c
+++ src/a_bat5.c
@@ -105,14 +105,33 @@ Var(mul       )("*", term)
 Var(div       )("/", term)
 Var(opr       )("(", term)
 Var(cpr       )(")", term)
+  // term capture terminal sentence operation 
+  // A recognizer corresponding to a construct p | q in the grammar is built by combining recognizers
+  // for p and q, using the parser combinator ‘orelse‘.
+  // When the composite recognizer is applied to index j, it applies p to j, then it applies q to j,
+  // and subsequently unites the resulting sets.:
+  // (p ‘orelse‘ q) j = unite (p j) (q j)
+  // e.g, assuming that the input is "ssss", then
+  // (empty ‘orelse‘ term_s) 2 => {2, 3}
+  //
+  // A composite recognizer corresponding to a sequence of recognizers p q on the right hand side of
+  // a grammar rule, is built by combining those recognizers using the parser combinator ‘thenS‘.
+  // When the composite recognizer is applied to an index j, it first applies p to j, then it applies
+  // q to each index in the set of results returned by p. It returns the union of these applications of q.
+  // (p ‘thenS‘ q) j = union (map q (p j))
+  // e.g., assuming that the input is "ssss", then
+  // (term_s ‘thenS‘ term_s) 1 => {3}
 Var(Exp       )("a", term,
                 opr, Exp, 1, thenS_n,
                      cpr, 1, thenS_n, 7, orelse_n,
                 Exp, mul, 1, thenS_n,
                      Exp, 1, thenS_n, 7, orelse_n,
-                Exp, div, 1, thenS_n, Exp, 1, thenS_n, 7, orelse_n,
-                Exp, mns, 1, thenS_n, Exp, 1, thenS_n, 7, orelse_n,
-                Exp, pls, 1, thenS_n, Exp, 1, thenS_n, 7, orelse_n,
+                Exp, div, 1, thenS_n,
+                     Exp, 1, thenS_n, 7, orelse_n,
+                Exp, mns, 1, thenS_n,
+                     Exp, 1, thenS_n, 7, orelse_n,
+                Exp, pls, 1, thenS_n,
+                     Exp, 1, thenS_n, 7, orelse_n,
                 Exp, variable)
 N(o_orelse_nn ) {
   R(p_t*, rο);
@@ -120,55 +139,22 @@ N(o_orelse_nn ) {
   R(p_t*, sο);
   R(Q_t, pos);
   Α(pos, sο, rο, oο, 2, os_soll_n) O; }
-N(o_orelse_n  ) { VLog; Α(os_soll_n, o_orelse_nn, and, dot, and) O; }
+N(o_orelse_n  ) { VLog; R(Q_t, wc); α -= wc, O; }
 N(o_thenS_n   ) { VLog; R(Q_t, wc); α -= wc, O; }
 N(o_empty     ) { VLog;      C(1); }
 N(o_term      ) { VLog; α--, C(1); }
-N(o_variable  ) { VLog; α--, O; }
+N(dump) {VLog; os_wordump(T()); }
+N(o_variable  ) { VLog;
+  Α(drop, dot, and, dump, and) O;
+}
 
 Nar(p_pith    )(o_orelse_n, o_thenS_n, o_term, o_empty, o_variable, 5, os_soll_n)
 NarP(rsoll    )(0, os_soll_n)
 NarP(ssoll    )(0, os_soll_n)
 Nar(example   )("a+a*a", 0, 5, 0, ssoll, rsoll, and, Exp, p_pith, and2, coll, and, os_wordump, and)
-
 Nar(init      )(god)
 
-NP(sar_nnnn) {
-  R(p_t*, soor);
-  R(p_t*, sand);
-  R(p_t*, snot);
-  R(p_t*, slhs);
-  Α(slhs, os_unsoll_free, dot, and,
-      snot, os_unsoll_free, dot, and,
-      sand, os_unsoll_free, dot, and,
-      soor, os_unsoll_free, dot, and, 0444, nar, 4+4+4+4+2, os_soll_n) O; }
-NP(sar_nnn ) {
-  R(Q_t, j);
-  R(p_t*, soor);
-  R(p_t*, sand);
-  R(p_t*, snot);
-  Α(dot, snot, sand, soor, sar_nnnn, and4) O;
-}
-NP(sar_nn  ) {
-  R(Q_t, j);
-  R(p_t*, soor);
-  R(p_t*, sand);
-  Α((j >> 16) & 0xff, os_soll_n, sand, soor, j, sar_nnn, and4) O;
-}
-NP(sar_n   ) {
-  R(Q_t, j);
-  R(p_t*, soor);
-  Α((j >>  8) & 0xff, os_soll_n, soor, j, sar_nn, and3) O; }
-NP(sar     ) {
-  R(Q_t, j);
-  Α((j >>  0) & 0xff, os_soll_n, j, sar_n, and2) O; }
-
-Nar(example2  )(god, 1, os_soll_n,
-                god, god, and,
-                god, god, and,
-                god, god, and, 0x030303, sar, os_unsoll_free, and, dot, and)
-
-Nar(მთავარი   )(example2)
+Nar(მთავარი   )(example)
 // clang-format off
 EN(tail,
 მთავარი,      exports);
