1f267d7a3f90f187c406c69def588a9c73947c76
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-06-03 10:36:11 +0400

    pith

--- src/a_empty.c
+++ src/a_empty.c
@@ -7,7 +7,39 @@ got,                L)IN(L,
 nar,          imports)
 
 Sargo(init)(god)
-static N(Main) { PLog; A(7) C(1); }
+N(one) { 
+  σ[α++].Q = 1;
+  ο[1].c(ο, α, ρ, σ);
+}
+N(plus) {
+  q_t r = σ[--α].q;
+  q_t l = σ[--α].q;
+  σ[α++].q = l + r;
+  ο[1].c(ο, α, ρ, σ);
+}
+N(and) {
+}
+N(ο0) {
+  σ[α-1].c(ο, α-1, ρ, σ);
+}
+N(ο1) {
+  σ[ρ+1].c(ο, α, ρ, σ);
+}
+N(two) {
+  σ[α++].c = one;
+  σ[α++].c = one;
+  σ[α++].c = and;
+  σ[α++].c = plus;
+  σ[α++].c = and;
+  ο[0].c(ο, α, ρ, σ);
+}
+N(g0d) { σ[ρ + 1].c(ο, α, ρ, σ); }
+
+N(Main) { A(god) ο[0].c(T()); }
+
+void Main_(p_t *ο, Q_t α, Q_t ρ, p_t *σ) {
+  σ[α++].v = (void *)(god), ο[0].c(ο, α, ρ, σ);
+}
 
 EN(tail,
 Main, exports)
