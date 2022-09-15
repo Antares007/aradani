5df845984f47cde61b9dc116f1e2bd0cc5b65b8d
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-09-15 07:25:39 +0400

    formated logn

--- src4/oars_logn.h
+++ src4/oars_logn.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <stdio.h>
-#define LOG printf("α:%lu ρ:%lu %s\n", α, ρ, __FUNCTION__)
+#define LOG printf("%2lu%5lu %s\n", α, ρ, __FUNCTION__)
 #undef N
 #define N(Name)                                                                \
   void Name##Nar(OARS);                                                        \
