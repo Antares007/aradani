d31a2a9edb5e06a3e0758bc2065b76f5a0238fce
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-09-18 06:45:54 +0400

    sync show

---
+++ src/a_show.c
@@ -0,0 +1,21 @@
+#include "arsi.h"
+// clang-format off
+                    IN(Gor,
+God,              L)IN(L,
+Got,              L)IN(L,
+load_pith,  imports);
+// clang-format on
+
+N(on_tick ) { C(1); }
+N(on_stdin) { C(1); }
+N(on_init ) {
+  A3("pith", Tail, load_pith) O;
+}
+N(on_draw ) { C(1); }
+
+// clang-format off
+                    EN(tail,
+on_draw,          L)EN(L,
+on_init,          L)EN(L,
+on_stdin,         L)EN(L,
+on_tick,    exports);
