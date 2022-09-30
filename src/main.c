4a764152d7dea8d540ba34afecb97ef8639bb400
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-09-30 05:42:16 +0400

    sync

--- src/main.c
+++ src/main.c
@@ -19,11 +19,11 @@ Got,                              L)E(L,
 "stdiofun", &stdiofun,      exports);
 // clang-format on
 
-N(root    ) { A1(exports) C(1); }
+N(root) { A1(exports) C(1); }
 
-N(ray_not ) {}
-N(ray_and ) { printf("%lu\n", σ[--α].q); }
-N(ray_oor ) {}
+N(ray_not) {}
+N(ray_and) { printf("%lu\n", σ[--α].q); }
+N(ray_oor) {}
 
 N(ls_exports) {
   RN(const char *, fname);
@@ -39,15 +39,12 @@ N(run_epoll) {
   RN(n_t, export);
   RN(n_t, nar);
   RN(const char *, name);
-  if (name[0] == '_') {
-    printf(">%s %lu\n", name, α);
-    A3(export, run_epoll, Τ[010]) O;
-  } else
+  if (name[0] == '_')
+    printf(">>> %s %lu\n", name, α), A3(export, run_epoll, Τ[010]) O;
+  else
     nar(T);
 }
-N(run_pith) {
-  A5(map_pith, Τ[0], Τ[010], run_epoll, Τ[010]) O;
-}
+N(run_bark) { A5(map_pith, Τ[0], Τ[010], run_epoll, Τ[010]) O; }
 #include "raylib.h"
 int main() {
   p_t ο[0x1000];
@@ -57,18 +54,17 @@ int main() {
   ο[--ρ].v = ray_not;
   ο[--ρ].v = ray_and;
   ο[--ρ].v = ray_oor;
-  A7(
-      "src/a_show.pith", root, run_pith, 
-      "src/a_pith2.pith", root, run_pith, Τ[030]
-     ) O;
+  A7("src/a_show.pith", root, run_bark, "src/a_pith2.pith", root, run_bark,
+     Τ[030])
+  O;
 
-  //const int screenWidth = 800;
-  //const int screenHeight = 450;
-  //InitWindow(screenWidth, screenHeight, "GUI Example!");
-  //SetTargetFPS(0);
-  //while (!WindowShouldClose()) {
-  //  BeginDrawing();
-  //  ClearBackground(RAYWHITE);
+  // const int screenWidth = 800;
+  // const int screenHeight = 450;
+  // InitWindow(screenWidth, screenHeight, "GUI Example!");
+  // SetTargetFPS(0);
+  // while (!WindowShouldClose()) {
+  //   BeginDrawing();
+  //   ClearBackground(RAYWHITE);
 
   //  DrawText("Sopo kargi gogoa", 240, 140, 20, GRAY);
 
