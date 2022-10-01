f799dab8a74ce063f2c024e02e8e15fbe9580e24
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-10-02 03:12:35 +0400

    extract show

--- src/a_show.c
+++ src/a_show.c
@@ -3,19 +3,36 @@
 #include "stdiofun.h"
 rayfun_t *r;
 stdiofun_t *s;
+n_t *ψ;
+n_t *ο, Got, God;
 // clang-format off
-                          IN(Gor,
-God,                    L)IN(L,
-Got,                    L)I(L,
+                          I(Gor,
+"God", God,             L)I(L,
+"Got", Got,             L)I(L,
 "raylibfun", r,         L)I(L,
-"stdiofun", s,    imports);
+"stdiofun", s,          L)I(L,
+"ο", ο,                 L)I(L,
+"ψ", ψ,           imports);
 // clang-format on
 
 N(on_tick2) { A(God) O; }
 N(_c      ) { A(5) C(1); }
-N(on_draw ) { 
-  s->printf("5\n");
-  A(7) C(1);
+N(one) { s->printf("1\n"), C(0); }
+N(on_draw ) { s->printf("5\n"), A(7) C(1); }
+N(show1) {
+  A17(
+    ο[0],
+      ο[0],
+        one, God, ψ[0001],
+      ο[3],
+    ο[5],
+      ο[0],
+        God, God, ψ[0010],
+      ο[3],
+        Got, God, ψ[0100],
+      ο[3],
+    ο[9]
+  ) O;
 }
 
 N(term) { C(1); }
@@ -26,6 +43,7 @@ N(_b) { A2("a", term) O; }
 // clang-format off
                           EN(tail,
 on_draw,                L)EN(L,
+show1,                  L)EN(L,
 _c,                     L)EN(L,
 _b,                     L)EN(L,
 _a,               exports);
--- src/main.c
+++ src/main.c
@@ -7,16 +7,19 @@ void Got(OANT);
 void God(OANT);
 void Gor(OANT);
 void map_pith(OANT);
-extern n_t ψ[];
+extern n_t ψ[01000];
+extern n_t ο[32];
 extern void *raylibfun[];
 extern void *stdiofun[];
 // clang-format off
-                                    EN(Got,
-God,                              L)EN(L,
-Gor,                              L)EN(L,
-Got,                              L)E(L,
+                                    E(Got,
+"God", God,                       L)E(L,
+"Gor", Gor,                       L)E(L,
+"Got", Got,                       L)E(L,
 "raylibfun", &raylibfun,          L)E(L,
-"stdiofun", &stdiofun,      exports);
+"stdiofun", &stdiofun,            L)E(L,
+"ο", &ο,                          L)E(L,
+"ψ", &ψ,                    exports);
 // clang-format on
 
 N(root) { A1(exports) C(1); }
@@ -42,42 +45,15 @@ N(run_epoll) {
 }
 N(run_bark) { A5(map_pith, ψ[0], ψ[010], run_epoll, ψ[010]) O; }
 #include "raylib.h"
-extern n_t ο[32];
 
 void queue_init();
 void next(OANT);
-N(ray_not) {}
-N(ray_and) {
-  if (τ)
-    printf("%lu\n", ν[τ - 1].q);
-  else
-    printf("\n");
-  α -= 3, next(T);
-}
-N(ray_oor) {}
 // clang-format off
-N(show1) {
-  A17(
-    ο[0],
-      ο[0],
-        Gor, God, ψ[0001],
-      ο[3],
-    ο[5],
-      ο[0],
-        God, God, ψ[0010],
-      ο[3],
-        Got, God, ψ[0100],
-      ο[3],
-    ο[9]
-  ) O;
-}
-N(show2) {
-  A7(
-    "src/a_show.pith",  root, run_bark,
-    "src/a_pith2.pith",  root, run_bark, ψ[030]
-  ) O;
-}
+N(ray_not) {}
+N(ray_and) { if (τ) printf("%lu\n", ν[τ - 1].q); else printf("\n"); α -= 3, next(T); }
+N(ray_oor) {}
 int main() {
+  queue_init();
   p_t ω[0x1000];
   Q_t α = sizeof(ω) / sizeof(*ω);
   p_t ν[0x1000];
@@ -85,8 +61,10 @@ int main() {
   ω[--α].v = ray_not;
   ω[--α].v = ray_and;
   ω[--α].v = ray_oor;
-  queue_init();
-  A3(show1, show2, ψ[010]) O;
+  A3(
+    "src/a_show.pith",  root, run_bark
+//    "src/a_pith2.pith",  root, run_bark, ψ[030]
+  ) O;
   // const int screenWidth = 800;
   // const int screenHeight = 450;
   // InitWindow(screenWidth, screenHeight, "GUI Example!");
