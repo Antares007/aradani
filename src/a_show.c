eeaea75c9f71841d7bb0ea35bdaad4f0ec4773f7
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-10-04 06:14:36 +0400

    init window

--- src/a_show.c
+++ src/a_show.c
@@ -11,46 +11,58 @@ n_t Got, God;
 "ρ", ρ,          L)I(L,
 "σ", σ,          ι);
 // clang-format on
+N(InitWindow) {
+  const int screenWidth = 800;
+  const int screenHeight = 450;
+  ρ->InitWindow(screenWidth, screenHeight, "GUI Example!");
+  ρ->SetTargetFPS(0);
+  C(1);
+}
+N(WindowShouldNotClose) { C(!ρ->WindowShouldClose()); }
+N(BeginDrawing) { ρ->BeginDrawing(); ρ->ClearBackground(RAYWHITE); C(1); }
+N(EndDrawing) { ρ->EndDrawing(); C(1); }
+N(Draw) { ρ->DrawText("aba mamao!", 240, 140, 44, GRAY); C(1); }
+N(loop) {
+  A9(WindowShouldNotClose, BeginDrawing, ψ[010],
+                                   Draw, ψ[010],
+                             EndDrawing, ψ[010],
+                                   loop, ψ[010]) O;
+}
+N(xxx) {
+  A3(InitWindow, loop, ψ[010]) O;
+}
+N(counter) {
+  //(
+  //  Δ.draw,
+  //    Δ.draw,
+  //    "+",
+  //    Δ.text,
+  //  Δ.button,
+  //    Δ.draw,
+  //    "-",
+  //    Δ.text,
+  //  Δ.button,
+  //  "0",
+  //  Δ.text,
 
-N(on_tick2) { A(God) O; }
-N(_c      ) { A(5) C(1); }
-N(one) { σ->printf("1\n"), C(0); }
-N(on_draw) { σ->printf("5\n"), A(7) C(1); }
-N(show) {
+  //  Σ.aba
+  //) O;
+}
+N(treeasnumbers) {
   A17(
     δ[0],
       δ[0],
-        one, God, ψ[0001],
+        God, God, ψ[0001],
       δ[3],
     δ[5],
       δ[0],
         God, God, ψ[0010],
       δ[3],
-        Got, God, ψ[0100],
+        Got, treeasnumbers, ψ[0100],
       δ[3],
     δ[9]
   ) O;
 }
-  // const int screenWidth = 800;
-  // const int screenHeight = 450;
-  // InitWindow(screenWidth, screenHeight, "GUI Example!");
-  // SetTargetFPS(0);
-  // while (!WindowShouldClose()) {
-  //   BeginDrawing();
-  //   ClearBackground(RAYWHITE);
-  //  DrawText("aba mamao!", 240, 140, 20, GRAY);
-  //  EndDrawing();
-  // }
-
-N(term) { C(1); }
-
-N(_a) { A2("b", term) O; }
-N(_b) { A2("a", term) O; }
-
 // clang-format off
-                          EN(ζ,
-on_draw,                L)EN(L,
-_a,                     L)EN(L,
-_c,                     L)EN(L,
-_b,                     L)EN(L,
-show,                   ε);
+           EN(ζ,
+xxx,     ε);
